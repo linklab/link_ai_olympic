62f780c6683f6b053c34203be337c5ff9f10743c
Youn-Hee Han <yh21.han@gmail.com>
2023-05-11 07:55:51 +0900

    ...

--- replay_buffer.py
+++ replay_buffer.py
@@ -167,7 +167,7 @@ class ReplayBuffer(IterableDataset):
 def _worker_init_fn(worker_id):
     seed = np.random.get_state()[1][0] + worker_id
     np.random.seed(seed)
-    random.seed(seed)
+    random.seed(int(seed))
 
 
 def make_replay_loader(replay_dir, max_size, batch_size, num_workers,
